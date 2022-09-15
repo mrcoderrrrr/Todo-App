26956781da38b0f56250827a2f9bd3c408959efb
mrcoderrrrr <psatardekar0192000@gmail.com>
2022-09-15 16:40:59 +0530

    test151

--- app/src/main/java/com/example/todoapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.todoapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.todoapp.R
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dataBinding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
